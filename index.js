ba1a8b8e92ab6a118a5a37ef56fab7cd914efb69
starkweb108 <starkweb2@gmail.com>
2022-09-06 18:40:55 +0100

    documentation added

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ const helmet = require('helmet');
 const rateLimiter = require('express-rate-limit');
 // ***
 
+// Swagger ui and Yaml for Api documentation
+
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 // *********************
 
 // express library import
@@ -53,6 +59,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Notes API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/authorize', userProfileRoutes);
